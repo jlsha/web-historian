2cf159cb254e057f7d66db7c4978b185f5df1149
jlsha <jenniferlaurasha@gmail.com>
2017-01-11 19:57:48 -0800

    pass 2/4 (Server Spec) pass 1 (Helper Spec)

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,34 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // URL Halpers
+  var readURL = `${archive.paths.siteAssets}/index.html`;
+  var targetURL = `${archive.paths.archivedSites}${req.url}`;
+
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.readFile(readURL, 'utf-8', function(err, data) {
+      if (err) {
+        throw err;
+      } else {
+        res.end(data);
+      }
+    }); 
+  } else if (req.method === 'GET' && req.url && req.url !== '/') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.readFile(targetURL, 'utf-8', function(err, data) {
+      if (err) {
+        throw err;
+      } else {
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+  }
 };
